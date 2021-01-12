4c2d011a3a7146382ef91999eae781292fe734b7
Leonel Rodriguez <leonellrodriguez@gmail.com>
2021-01-12 17:57:25 -0500

    Hasta Base de datos

---
+++ resources/views/cursos/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.plantilla')
+
+@section('title', 'Create')
+@section('content')
+    <h1>En esta pagina podras crear el curso</h1>
+@endsection
+
---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.plantilla')
+
+@section('title', 'Home')
+
+@section('content')
+    <h1>Bienvenidos al Home Controller!</h1>
+@endsection
+
